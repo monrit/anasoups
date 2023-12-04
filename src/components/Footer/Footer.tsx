16222d8615c188a882d7ab4f8a8ec1d2a17d1c36
Dmytro Kataiev <94922492+monrit@users.noreply.github.com>
2023-12-04 12:35:22 +0200

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 const Footer: FC = () => {
   return (
     <footer className={styles.footer}>
-      <h1 className={styles.rights}>AnaSoups™ all rights reserved</h1>
+      <h1 className={styles.rights}>LeoSoups™ all rights reserved</h1>
       <h2 className={styles.copyright}>2023 Copyright ©</h2>
     </footer>
   );
